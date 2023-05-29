a5bec8a79b8c1535402eebe9b196eed2d9d2b9b9
tizianomegnini <134891302+tizianomegnini@users.noreply.github.com>
2023-05-29 13:22:12 -0300

    Add files via upload

---
+++ multiplo.c
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <srting>
+using namespace std;
+
+bool EsMultiploDe17 (int n);
+int main()
+{
+int num;
+string msg ="NO ES MULTIPLO";
+cout<<"IIngrese un nùmero"<<endl;
+cin>>num
+	if(EsMultiploDe17(num))
+	{
+		msg= "ES MULTIPLO";
+	}
+	cout<<msg<<endl;
+    return 0;
+}
+
+bool EsMultiploDe17(int n)
+{bool flag=false;
+    if(n%17==0))
+    {
+        flag=true;
+    }
+}
